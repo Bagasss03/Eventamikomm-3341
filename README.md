21c9b81b3313cb7c6cb2882d59b4b92535cd4293
Bagas Imam Santoso <bagas.imam03@students.amikom.ac.id>
2026-04-17 14:55:31 +0700

    Tugas 2

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ Tugas 1
 Bagas Imam Santoso - 24.12.3341
 
 https://eventamikomm-3341-main-6vywhg.free.laravel.cloud/
+#Link menuju profil https://eventamikomm-3341-main-6vywhg.free.laravel.cloud/profil
+#Link menuju katalog https://eventamikomm-3341-main-6vywhg.free.laravel.cloud/katalog
+#Link menuju bantuan https://eventamikomm-3341-main-6vywhg.free.laravel.cloud/bantuan
+#Link menuju kontak https://eventamikomm-3341-main-6vywhg.free.laravel.cloud/kontak
+
+Tugas 2
+1. Halaman Dashboard : http://127.0.0.1:8000/admin-dashboard.html
+2. Halaman Event :
+3. Halaman Transaksi : 
+
+4. Halaman Beranda : http://127.0.0.1:8000/index.html#events
+5. Halaman Detail Event : http://127.0.0.1:8000/event-detail.html
+6. Halaman Chheckout :  
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Admin\DashboardController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/event/1', [EventController::class,'show'])->name('events.show');
+Route::get('/checkout', [EventController::class,'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+Route::get('/', [DashboardController::class,'index'])->name('dashboard');
+Route::get('/events', [EventController::class,'indexAdmin'])->name('events.index');
+// dan seterusnya...
+});
 
 // Rute Home (Bawaan Laravel atau buat view sendiri)
 Route::get('/', function () {
-    return view('profil'); // Pastikan file welcome.blade.php ada
+    return view('welcome'); // Pastikan file welcome.blade.php ada
 });
 
 // Rute Kontak (Dari tugas sebelumnya)
@@ -24,3 +37,4 @@ Route::get('/katalog', function () {
 Route::get('/bantuan', function () {
     return view('bantuan');
 });
+
