e89cc116cd568d78c2c46507ee2c7d5ddbe54df2
Bagas Imam Santoso <bagas.imam03@students.amikom.ac.id>
2026-05-08 20:52:01 +0700

    Tugas 4

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,10 @@ Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
 Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
 Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/events', [EventController::class, 'indexAdmin'])->name('events.index');
 
 // dan seterusnya...
-});
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('events', EventAdminController::class);
 });
