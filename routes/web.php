f8db9d3c3b787b06b490bda0dc59574109a62403
Bagas Imam Santoso <bagas.imam03@students.amikom.ac.id>
2026-05-08 17:54:06 +0700

    Tugas 4

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,46 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\EventController as AdminEventController;
-use App\Http\Controllers\EventController;
+// use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\Admin\EventController as EventAdminController;
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('events', EventAdminController::class);
-});
-
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/profil', function () {
-    return view('profil');
-})->name('profil');
-Route::get('/katalog', function () {
-    return view('katalog');
-})->name('katalog');
-Route::get('/bantuan', function () {
-    return view('bantuan');
-})->name('bantuan');
-Route::get('/kontak', function () {
-    return view('contact');
-})->name('kontak');
-
 Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
 Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
 Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
 
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/events', [EventController::class, 'indexAdmin'])->name('events.index');
 
+// dan seterusnya...
+});
+Route::prefix('admin')->name('admin.')->group(function () {
+Route::resource('events', EventAdminController::class);
+});
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-//      Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-//     // Route::get('/events', [AdminEventController::class, 'indexAdmin'])->name('events.index');
+Route::get('/tentang', function() {
+    return '<h1>Ini adalah halaman tentang aplikasi Event Hub</h1>';
+});
 
-//     Route::resource('events', EventAdminController::class);
-// });
+Route::get('/kontak', function() {
+    return view('contact');
+});
+
+Route::get('/profil', function(){
+    return view('profil');
+});
+
+Route::get('/katalog', function(){
+    return view('katalog');
+});
+
+Route::get('/bantuan', function(){
+    return view('bantuan');
+});
